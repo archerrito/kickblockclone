545144a4f15136ad80d26a6d2013a4bb783b2b03
Archer Hobson <supremeoverlord@Archers-MBP.attlocal.net>
2018-09-05 00:23:52 -0500

    make two column layout with button and list of contracts

--- pages/index.js
+++ pages/index.js
@@ -34,12 +34,13 @@ class CampaignIndex extends Component {
           href="//cdnjs.cloudflare.com/ajax/libs/semantic-ui/2.3.3/semantic.min.css">
         </link>
         <h3>Open Campaigns</h3>
-        {this.renderCampaigns()}
         <Button
+          floated="right"
           content="Create Campaign"
           icon="add circle"
           primary
         />
+        {this.renderCampaigns()}
       </div>
       </Layout>
     );
