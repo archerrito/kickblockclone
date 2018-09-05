cec3b852bc1c9c11dde41abb6af3b56de9fdbb0e
Archer Hobson <supremeoverlord@Archers-MBP.attlocal.net>
2018-09-05 13:19:09 -0500

    add server.js, route.js files to account for custom navigation

---
+++ routes.js
@@ -0,0 +1,7 @@
+//define our different routes
+//returns funciton, immediately invoked
+const routes = require('next-routes')();
+
+module.exports = routes;
+// //for routes with custom tokes
+// routes.add('...', '...');
