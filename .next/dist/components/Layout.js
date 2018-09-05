7e7989bdc83448f998df8c5c51d0e8d471dfeb12
Archer Hobson <supremeoverlord@Archers-MBP.attlocal.net>
2018-09-05 00:17:44 -0500

    style container and header top margin

--- .next/dist/components/Layout.js
+++ .next/dist/components/Layout.js
@@ -8,6 +8,8 @@ var _react = require('react');
 
 var _react2 = _interopRequireDefault(_react);
 
+var _semanticUiReact = require('semantic-ui-react');
+
 var _Header = require('./Header');
 
 var _Header2 = _interopRequireDefault(_Header);
@@ -17,16 +19,16 @@ function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { de
 var _jsxFileName = '/Users/supremeoverlord/Desktop/kickstart-blockchain-clone/components/Layout.js';
 
 exports.default = function (props) {
-    return _react2.default.createElement('div', {
-        __source: {
-            fileName: _jsxFileName,
-            lineNumber: 6
-        }
-    }, _react2.default.createElement(_Header2.default, {
+    return _react2.default.createElement(_semanticUiReact.Container, {
         __source: {
             fileName: _jsxFileName,
             lineNumber: 7
         }
+    }, _react2.default.createElement(_Header2.default, {
+        __source: {
+            fileName: _jsxFileName,
+            lineNumber: 8
+        }
     }), props.children);
 };
-//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbImNvbXBvbmVudHMvTGF5b3V0LmpzIl0sIm5hbWVzIjpbIlJlYWN0IiwiSGVhZGVyIiwicHJvcHMiLCJjaGlsZHJlbiJdLCJtYXBwaW5ncyI6Ijs7Ozs7O0FBQUEsQUFBTzs7OztBQUNQLEFBQU8sQUFBWSxBQUVuQjs7Ozs7Ozs7a0JBQWUsVUFBQSxBQUFDLE9BQVUsQUFDdEI7MkJBQ0ksY0FBQTs7c0JBQUE7d0JBQUEsQUFDQTtBQURBO0FBQUEsS0FBQSxrQkFDQSxBQUFDOztzQkFBRDt3QkFEQSxBQUNBLEFBQ0M7QUFERDtBQUFBLGNBRkosQUFDSSxBQUVPLEFBR2Q7QUFQRCIsImZpbGUiOiJMYXlvdXQuanMiLCJzb3VyY2VSb290IjoiL1VzZXJzL3N1cHJlbWVvdmVybG9yZC9EZXNrdG9wL2tpY2tzdGFydC1ibG9ja2NoYWluLWNsb25lIn0=
+//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbImNvbXBvbmVudHMvTGF5b3V0LmpzIl0sIm5hbWVzIjpbIlJlYWN0IiwiQ29udGFpbmVyIiwiSGVhZGVyIiwicHJvcHMiLCJjaGlsZHJlbiJdLCJtYXBwaW5ncyI6Ijs7Ozs7O0FBQUEsQUFBTzs7OztBQUNQLEFBQVM7O0FBQ1QsQUFBTyxBQUFZLEFBRW5COzs7Ozs7OztrQkFBZSxVQUFBLEFBQUMsT0FBVSxBQUN0QjsyQkFDSSxBQUFDOztzQkFBRDt3QkFBQSxBQUNBO0FBREE7QUFBQSxLQUFBLGtCQUNBLEFBQUM7O3NCQUFEO3dCQURBLEFBQ0EsQUFDQztBQUREO0FBQUEsY0FGSixBQUNJLEFBRU8sQUFHZDtBQVBEIiwiZmlsZSI6IkxheW91dC5qcyIsInNvdXJjZVJvb3QiOiIvVXNlcnMvc3VwcmVtZW92ZXJsb3JkL0Rlc2t0b3Ava2lja3N0YXJ0LWJsb2NrY2hhaW4tY2xvbmUifQ==
