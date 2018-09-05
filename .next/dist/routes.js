d7196ae1cc3954ffa718d1302f769dd8d4245ea9
Archer Hobson <supremeoverlord@Archers-MBP.attlocal.net>
2018-09-05 14:22:44 -0500

    handle routing of view campaign and new

--- .next/dist/routes.js
+++ .next/dist/routes.js
@@ -4,7 +4,11 @@
 //returns funciton, immediately invoked
 var routes = require('next-routes')();
 
+//colon represents wildcard variable
+//second argument is component
+routes.add('/campaigns/new', 'campaigns/new').add('/campaigns/:address', '/campaigns/show');
+
 module.exports = routes;
 // //for routes with custom tokes
 // routes.add('...', '...');
-//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbInJvdXRlcy5qcyJdLCJuYW1lcyI6WyJyb3V0ZXMiLCJyZXF1aXJlIiwibW9kdWxlIiwiZXhwb3J0cyJdLCJtYXBwaW5ncyI6Ijs7QUFBQTtBQUNBO0FBQ0EsSUFBTSxTQUFTLEFBQWY7O0FBRUEsT0FBTyxBQUFQLFVBQWlCLEFBQWpCO0FBQ0E7QUFDQSIsImZpbGUiOiJyb3V0ZXMuanMiLCJzb3VyY2VSb290IjoiL1VzZXJzL3N1cHJlbWVvdmVybG9yZC9EZXNrdG9wL2tpY2tzdGFydC1ibG9ja2NoYWluLWNsb25lIn0=
+//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbInJvdXRlcy5qcyJdLCJuYW1lcyI6WyJyb3V0ZXMiLCJyZXF1aXJlIiwiYWRkIiwibW9kdWxlIiwiZXhwb3J0cyJdLCJtYXBwaW5ncyI6Ijs7QUFBQTtBQUNBO0FBQ0EsSUFBTSxTQUFTLEFBQWY7O0FBRUE7QUFDQTtBQUNBLE9BQ0ssQUFETCxJQUNTLEFBRFQsa0JBQzJCLEFBRDNCLGlCQUVLLEFBRkwsSUFFUyxBQUZULHVCQUVnQyxBQUZoQzs7QUFJQSxPQUFPLEFBQVAsVUFBaUIsQUFBakI7QUFDQTtBQUNBIiwiZmlsZSI6InJvdXRlcy5qcyIsInNvdXJjZVJvb3QiOiIvVXNlcnMvc3VwcmVtZW92ZXJsb3JkL0Rlc2t0b3Ava2lja3N0YXJ0LWJsb2NrY2hhaW4tY2xvbmUifQ==
--- routes.js
+++ routes.js
@@ -2,6 +2,12 @@
 //returns funciton, immediately invoked
 const routes = require('next-routes')();
 
+//colon represents wildcard variable
+//second argument is component
+routes
+    .add('/campaigns/new', 'campaigns/new')
+    .add('/campaigns/:address', '/campaigns/show');
+
 module.exports = routes;
 // //for routes with custom tokes
 // routes.add('...', '...');
