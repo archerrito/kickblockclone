ca110d5d264955e27fb8983c945b1b1eab7baa0e
Archer Hobson <supremeoverlord@Archers-MBP.attlocal.net>
2018-09-05 00:38:28 -0500

    css fix for link tag

--- .next/dist/components/Layout.js
+++ .next/dist/components/Layout.js
@@ -10,6 +10,10 @@ var _react2 = _interopRequireDefault(_react);
 
 var _semanticUiReact = require('semantic-ui-react');
 
+var _head = require('next/dist/lib/head.js');
+
+var _head2 = _interopRequireDefault(_head);
+
 var _Header = require('./Header');
 
 var _Header2 = _interopRequireDefault(_Header);
@@ -17,18 +21,30 @@ var _Header2 = _interopRequireDefault(_Header);
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
 var _jsxFileName = '/Users/supremeoverlord/Desktop/kickstart-blockchain-clone/components/Layout.js';
+//is a component
 
 exports.default = function (props) {
     return _react2.default.createElement(_semanticUiReact.Container, {
         __source: {
             fileName: _jsxFileName,
-            lineNumber: 7
+            lineNumber: 9
         }
-    }, _react2.default.createElement(_Header2.default, {
+    }, _react2.default.createElement(_head2.default, {
         __source: {
             fileName: _jsxFileName,
-            lineNumber: 8
+            lineNumber: 10
+        }
+    }, _react2.default.createElement('link', {
+        rel: 'stylesheet',
+        href: '//cdnjs.cloudflare.com/ajax/libs/semantic-ui/2.3.3/semantic.min.css', __source: {
+            fileName: _jsxFileName,
+            lineNumber: 11
+        }
+    })), _react2.default.createElement(_Header2.default, {
+        __source: {
+            fileName: _jsxFileName,
+            lineNumber: 16
         }
     }), props.children);
 };
-//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbImNvbXBvbmVudHMvTGF5b3V0LmpzIl0sIm5hbWVzIjpbIlJlYWN0IiwiQ29udGFpbmVyIiwiSGVhZGVyIiwicHJvcHMiLCJjaGlsZHJlbiJdLCJtYXBwaW5ncyI6Ijs7Ozs7O0FBQUEsQUFBTzs7OztBQUNQLEFBQVM7O0FBQ1QsQUFBTyxBQUFZLEFBRW5COzs7Ozs7OztrQkFBZSxVQUFBLEFBQUMsT0FBVSxBQUN0QjsyQkFDSSxBQUFDOztzQkFBRDt3QkFBQSxBQUNBO0FBREE7QUFBQSxLQUFBLGtCQUNBLEFBQUM7O3NCQUFEO3dCQURBLEFBQ0EsQUFDQztBQUREO0FBQUEsY0FGSixBQUNJLEFBRU8sQUFHZDtBQVBEIiwiZmlsZSI6IkxheW91dC5qcyIsInNvdXJjZVJvb3QiOiIvVXNlcnMvc3VwcmVtZW92ZXJsb3JkL0Rlc2t0b3Ava2lja3N0YXJ0LWJsb2NrY2hhaW4tY2xvbmUifQ==
+//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbImNvbXBvbmVudHMvTGF5b3V0LmpzIl0sIm5hbWVzIjpbIlJlYWN0IiwiQ29udGFpbmVyIiwiSGVhZCIsIkhlYWRlciIsInByb3BzIiwiY2hpbGRyZW4iXSwibWFwcGluZ3MiOiI7Ozs7OztBQUFBLEFBQU87Ozs7QUFDUCxBQUFTOztBQUVULEFBQU87Ozs7QUFDUCxBQUFPLEFBQVksQUFFbkI7Ozs7Ozs7QUFKQTs7a0JBSWUsVUFBQSxBQUFDLE9BQVUsQUFDdEI7MkJBQ0ksQUFBQzs7c0JBQUQ7d0JBQUEsQUFDQTtBQURBO0FBQUEsS0FBQSxrQkFDQSxBQUFDOztzQkFBRDt3QkFBQSxBQUNBO0FBREE7QUFBQTthQUNBLEFBQ0ksQUFDSjtjQUZBLEFBRUs7c0JBRkw7d0JBRkEsQUFDQSxBQUNBLEFBS0E7QUFMQTtBQUNBLHlCQUlBLEFBQUM7O3NCQUFEO3dCQVBBLEFBT0EsQUFDQztBQUREO0FBQUEsY0FSSixBQUNJLEFBUU8sQUFHZDtBQWJEIiwiZmlsZSI6IkxheW91dC5qcyIsInNvdXJjZVJvb3QiOiIvVXNlcnMvc3VwcmVtZW92ZXJsb3JkL0Rlc2t0b3Ava2lja3N0YXJ0LWJsb2NrY2hhaW4tY2xvbmUifQ==
